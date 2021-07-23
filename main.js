34bd1a2dc5831f047385980e92be6310d35c69f3
Esther Choi <81777226+CatGirl5773@users.noreply.github.com>
2021-07-23 15:29:54 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX = 0;
+noseY = 0;
+
 function preload() {
+    lips = loadImage('https://i.postimg.cc/bwX5KvtQ/lips.png');
 }
 
 function setup() {
@@ -14,6 +18,7 @@ function setup() {
 
 function draw() {
     image(video, 0, 0, 300, 300);
+    image(lips, noseX, noseY, 25, 20);
 }
 
 function take_snapshot() {
@@ -27,6 +32,8 @@ function modelLoaded() {
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        noseX = results[0].pose.nose.x - 12;
+        noseY = results[0].pose.nose.y + 13;
         console.log("Nose x = " + results[0].pose.nose.x);
         console.log("Nose y = " + results[0].pose.nose.y);
     }
